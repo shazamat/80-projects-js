21bc45ff91e664e4ead2424fd2b1375d8e9dd80a
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-06-05 16:55:33 +0500

    complete

--- 42-is-array-sorted-or-not/start.js
+++ 42-is-array-sorted-or-not/start.js
@@ -16,6 +16,23 @@ const b = [4, 10, 14, 25, 25, 50]
 const c = [150, 132, 80, 40]
 const d = [15, 26, 10, 23, 85]
 
+
+const arraySortInfo = inputArray => {
+    if (inputArray.some((element) => typeof element !== 'number')) {
+        return 'Некоторые элементы не являются числами';
+    }
+    
+    if(inputArray.every((el, index) => index > 0 ? el >= inputArray[index - 1] : true)) {
+        return 'Массив отсортирован по возрастанию';
+    }
+
+    if(inputArray.every((el, index) => index > 0 ? el <= inputArray[index - 1] : true)) {
+        return 'Массив отсортирован по убыванию';
+    }
+    
+    return 'Массив не отсортирован';
+}
+
 console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
 console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
 console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
