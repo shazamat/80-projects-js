2e9776e63510f8b8403edad418713d7a5f3daefe
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-06-06 19:14:18 +0500

    Complete 52

--- 52-default-function-parameters/start.js
+++ 52-default-function-parameters/start.js
@@ -14,6 +14,11 @@
  * Внимательно сравните свои результаты с результатами тестовых вызовов
  */
 
+function weatherForecast(city, weather = 'Отличная погода!') {
+    return `Прогноз погоды для города ${city}: ${weather}`;
+}
+
+
 console.log(weatherForecast('Dubai', 'Солнечно'))
 // Прогноз погоды для города Dubai: Солнечно
 
