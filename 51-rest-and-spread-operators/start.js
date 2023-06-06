8f629d417ae1e4e6c46b5d7b5f6e5b43e56de091
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-06-06 18:57:57 +0500

    Complete

--- 51-rest-and-spread-operators/start.js
+++ 51-rest-and-spread-operators/start.js
@@ -18,16 +18,24 @@ const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
 const scores3 = [1.3, 2.5, 1.9]
 const scores4 = ['abc', 1.3, true, 2.5, 1.9]
 
-console.log(meanScore(/* все элементы из массива "scores1" */))
+
+function meanScore(...numbers) {
+    if (numbers.some((num) => typeof num !== 'number')) {
+        return 'Все аргументы в вызове функции должны быть числами!';
+    }
+  return numbers.reduce((a, b) => a + b / numbers.length, 0).toFixed(2);
+}
+
+
+
+
+console.log(meanScore(...scores1));
 // 1.93
 
-console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+console.log(meanScore(...scores1, ...scores2));
 // 2.8
 
-console.log(
-  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
-)
+console.log(meanScore(...scores1, ...scores2, ...scores3));
 // 2.59
 
-console.log(meanScore(/* все элементы из массива "scores4" */))
-// Все аргументы в вызове функции должны быть числами!
+console.log(meanScore(...scores4));
