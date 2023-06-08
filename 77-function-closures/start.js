0d526bf2d2ab4d15fa2a6b5b23ea89135dbd5497
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-06-08 16:22:22 +0500

    Complete

--- 77-function-closures/start.js
+++ 77-function-closures/start.js
@@ -14,6 +14,24 @@
  *  - changeGreeting
  */
 
+const createGreeting = () => {
+    let greetingString = 'Hey, this is';
+
+    function greet(name) {
+        return `${greetingString} ${name}`
+    }
+
+    function changeGreeting(newGreeting) {
+        greetingString = newGreeting
+    }
+
+    return {
+        greet,
+        changeGreeting
+    }
+}
+
+
 const greeting1 = createGreeting()
 
 console.log(greeting1.greet('Bob'))
